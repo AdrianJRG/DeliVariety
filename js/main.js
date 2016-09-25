5b93de346052120118c9630f64b164b057e9f976
AdrianJRG <adrian.guillot@gmail.com>
2016-09-25 12:47:15 +0200

    Added back to top button (jQuery). Added images for reservation tables

--- js/main.js
+++ js/main.js
@@ -17,6 +17,23 @@ $(document).ready(function () {
         $body.removeClass("noScroll");
     }
 
+    jQuery(document).ready(function() {
+        var offset = 250;
+        var duration = 300;
+        jQuery(window).scroll(function() {
+            if (jQuery(this).scrollTop() > offset) {
+                jQuery('.to-top').fadeIn(duration);
+            } else {
+                jQuery('.to-top').fadeOut(duration);
+            }
+        });
+        jQuery('.to-top').click(function(event) {
+            event.preventDefault();
+            jQuery('html, body').animate({scrollTop: 0}, duration);
+            return false;
+        })
+    });
+
     $bgMenu.click(showMenu);
     $btnClose.click(closeMenu);
 })
